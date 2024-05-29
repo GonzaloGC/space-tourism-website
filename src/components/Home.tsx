cb361f141e2dff8bff9d3304e7a12e40181c1599
Gonzalo <elias.gonzaloguerrero@gmail.com>
2024-05-28 21:21:44 -0400

    first commit, started proyect

---
+++ src/components/Home.tsx
@@ -0,0 +1,7 @@
+import './Home.css'
+
+export const Home =()=>{
+  return(
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil maiores aperiam magni obcaecati similique eius, molestiae, impedit dolores omnis beatae excepturi quam atque tenetur delectus nostrum facilis. Dolorum, voluptatum doloribus!</p>
+  )
+}
